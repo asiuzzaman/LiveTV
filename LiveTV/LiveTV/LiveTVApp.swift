82abdb8686911a824c75e00ba56adc5dac0e0c78
Md. Asiuzzaman <md.asiuzzaman@portonics.com>
2025-12-27 20:25:45 +0600

    Initiate Live TV project

---
+++ LiveTV/LiveTV/LiveTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTVApp.swift
+//  LiveTV
+//
+//  Created by Md. Asiuzzaman on 27/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
